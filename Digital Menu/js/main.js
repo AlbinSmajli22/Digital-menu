7ddc3f342fd326cf3dafb9f753952ad4f4402c8c
Albin Smajli <64081932+AlbinSmajli22@users.noreply.github.com>
2022-07-18 16:13:38 +0200

    Add files via upload

---
+++ Digital Menu/js/main.js
@@ -0,0 +1,501 @@
+var array = [];
+var array2=[];
+
+
+$('#btn').click(function(){
+    var input = 'Pizza Margarita e Vogel';
+    var cmimi=2.5;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#btnm').click(function(){
+    var input = 'Pizza Margarita e Mesme';
+    var cmimi=3;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#btnma').click(function(){
+    var input = 'Pizza Margarita e Madhe';
+    var cmimi=4;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');    
+        array.push(cmimi);
+})
+//<<<<<<
+$('#btn2').click(function(){
+    var input = 'Pizza Pepperoni e Vogel';
+    var cmimi=2.5;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#btnm2').click(function(){
+    var input = 'Pizza Pepperoni e Mesme';
+    var cmimi=3;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#btnma2').click(function(){
+    var input = 'Pizza Pepperoni e Madhe';
+    var cmimi=4;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+//>>>>>>>
+
+
+$('#btn3').click(function(){
+    var input = 'Pizza Seafood e Vogel';
+    var cmimi=2.5;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#btnm3').click(function(){
+        var input = 'Pizza Seafood e Mesme';
+        var cmimi=3;
+       
+            $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+            array.push(cmimi);
+    })
+$('#btnma3').click(function(){
+        var input = 'Pizza Seafood e Madhe';
+        var cmimi=4;
+ 
+            $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+            array.push(cmimi);
+    })
+//>>>>>>>>>
+
+$('#btn4').click(function(){
+    var input = 'Pizza Vegetarian e vogel';
+    var cmimi=2.5;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#btnm4').click(function(){
+    var input = 'Pizza Vegetarian e Mesme';
+    var cmimi=3;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#btnma4').click(function(){
+    var input = 'Pizza Vegetarian e Madhe';
+    var cmimi=4;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+//>>>>>>>
+$('#btn5').click(function(){
+    var input = 'Pizza Vegetarian e vogel';
+    var cmimi=2.5;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#btnm5').click(function(){
+    var input = 'Pizza Vegetarian e Mesme';
+    var cmimi=3;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#btnma5').click(function(){
+    var input = 'Pizza Vegetarian e Madhe';
+    var cmimi=4;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#sallata1').click(function(){
+    var input = 'Sallat Greke';
+    var cmimi=1.5;
+       
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#sallata2').click(function(){
+    var input = 'Sallat Shope';
+    var cmimi=1.5;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#sallata3').click(function(){
+    var input = 'Sallat Mix';
+    var cmimi=3;
+      
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#sallata4').click(function(){
+    var input = 'Sallat Pule';
+    var cmimi=2.5;
+    
+        $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+        array.push(cmimi);
+})
+$('#sallata5').click(function(){
+    var input = 'Sallat Cezar';
+    var cmimi=2;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pasta1').click(function(){
+    var input = 'Makarona Bolonez';
+    var cmimi=2;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pasta2').click(function(){
+    var input = 'Makarona Napoli';
+    var cmimi=2;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pasta3').click(function(){
+    var input = 'Makarona Me Proshutë';
+    var cmimi=2;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pasta4').click(function(){
+    var input = 'Makarona Carbonara';
+    var cmimi=2.5;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pasta5').click(function(){
+    var input = 'Makarona Në Tavë';
+    var cmimi=2.5;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#skara1').click(function(){
+    var input = 'File Pule Natyrale';
+    var cmimi=2.5;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#skara2').click(function(){
+    var input = 'File Pule Me Sos Kërpudhe';
+    var cmimi=3;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#skara3').click(function(){
+    var input = 'Bërxoll Vieneze';
+    var cmimi=3;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#skara4').click(function(){
+    var input = 'Bërxoll Speciale në Furrë';
+    var cmimi=3;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#skara5').click(function(){
+    var input = 'Kombinim Mishi';
+    var cmimi=7;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#rizoto1').click(function(){
+    var input = 'Rizoto me Mish Pule';
+    var cmimi=3;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#rizoto2').click(function(){
+    var input = 'Rizoto me Perime';
+    var cmimi=2.6;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+
+$('#rizoto3').click(function(){
+    var input = 'Rizoto me Kerpurdha';
+    var cmimi=3;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+
+$('#rizoto4').click(function(){
+    var input = 'Grill Chicken Burger';
+    var cmimi=1.8;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+
+$('#rizoto5').click(function(){
+    var input = 'Burger classic';
+    var cmimi=1.8;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+
+$('#rizoto6').click(function(){
+    var input = 'Burger Charlie';
+    var cmimi=2.5;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#sanwich1').click(function(){
+    var input = 'Sanwich me Përshutë';
+    var cmimi=1.7;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#sanwich2').click(function(){
+    var input = 'Sanwich Tuna';
+    var cmimi=1.7;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#sanwich3').click(function(){
+    var input = 'Sanwich Vegjetarian';
+    var cmimi=1.7;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#sanwich4').click(function(){
+    var input = 'Sanwich Special';
+    var cmimi=2.5;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#sanwich5').click(function(){
+    var input = 'Sanwich me Bukë Shtëpie';
+    var cmimi=2;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#omlet1').click(function(){
+    var input = 'Omlet Natyral';
+    var cmimi=1.5;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#omlet2').click(function(){
+    var input = 'Omlet me Përshutë';
+    var cmimi=1.7;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#omlet3').click(function(){
+    var input = 'Omlet Mix';
+    var cmimi=1.8;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#omlet4').click(function(){
+    var input = 'Omlet me Perime';
+    var cmimi=1.7;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#omlet5').click(function(){
+    var input = 'Omlet me Djath';
+    var cmimi=1.7;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#supa1').click(function(){
+    var input = 'Supë Pule';
+    var cmimi=1.5;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#supa2').click(function(){
+    var input = 'Supë Domatesh';
+    var cmimi=1.5;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#supa3').click(function(){
+    var input = 'Supë Broccoli';
+    var cmimi=1.5;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#supa4').click(function(){
+    var input = 'Supë Peshku';
+    var cmimi=1.7;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+/////////////////////////////////////
+$('#pija1').click(function(){
+    var input = 'Coca Cola';
+    var cmimi=1;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija2').click(function(){
+    var input = 'Fanta';
+    var cmimi=1;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija3').click(function(){
+    var input = 'Schwepps';
+    var cmimi=1;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija4').click(function(){
+    var input = 'Sprite';
+    var cmimi=1;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija5').click(function(){
+    var input = 'RED BULL';
+    var cmimi=1;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija6').click(function(){
+    var input = 'Pjeshkë';
+    var cmimi=1;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija7').click(function(){
+    var input = 'Dredhëzë';
+    var cmimi=1;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija8').click(function(){
+    var input = 'Vishnje';
+    var cmimi=1;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija9').click(function(){
+    var input = 'Mollë';
+    var cmimi=1;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija10').click(function(){
+    var input = 'Portokallë';
+    var cmimi=1;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija11').click(function(){
+    var input = 'Macchiato e Vogel';
+    var cmimi=0.5;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija12').click(function(){
+    var input = 'Macchiato e Madh';
+    var cmimi=0.7;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija13').click(function(){
+    var input = 'Exspresso';
+    var cmimi=0.5;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+})
+$('#pija14').click(function(){
+    var input = 'Cappuccino';
+    var cmimi=0.7;
+
+      $('#Porosia').append('<li>' + input+" "+ cmimi +"€"+ '</li>');
+      array.push(cmimi);
+   
+    })
+$('#pija15').click(function(){
+    var input = 'Latte';
+    var cmimi=1;
+
+      $('#Porosia').append('<li>' + input+" "+'<span>'+ cmimi +'</span>'+"€"+  '</li>');
+      array.push(cmimi);
+})    
+
+    
+$('#fshi').click(function(){
+    $('li:last').remove();
+    array.pop();
+    $('#totali2').empty()
+})
+$('#fshi2').click(function(){
+    $('#Porosia li').remove();
+    array.length=0;
+    $('#totali2').empty()
+})
+    
+    
+    $('#totali').click(function(){
+      var sum = array.reduce(function(a, b){
+        return a + b;
+    }, 0);
+      $('#totali2').append(sum + "€");
+      console.log(array);
+    })
+  
+      
+   
+
+
+
+
+
